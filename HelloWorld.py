615f9f1f726e4256498d7dd55f2a0a934b641300
rstokken <63981899+rstokken@users.noreply.github.com>
2021-05-06 23:52:57 -0400

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,32 @@
+
+###########################################################
+### HOST: GitHubAccount / rstokken / Hello-World
+# FILE: HelloWorld.py
+#
+# INSPIRED BY:
+#
+###########################################################
+# DESCRIPTION
+###########################################################
+# The usual Hello World program to get started and learn the procedures 
+# of the GitHub hosting site. 
+#
+# USAGE:
+#
+# EXAMPLE:
+#
+###########################################################
+
+
+###########################################################
+# HISTORY - END
+###########################################################
+### RAS = Richard A. Stokken
+#
+#
+# 2021.05.06 rstokken : Created file.
+#
+#
+##### HISTORY - START #####################################
+
+print("Hello World")
